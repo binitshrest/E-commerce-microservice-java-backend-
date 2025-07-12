7b89ac19760b94a653cd05665fcd9225e13b33b4
Binit Shrestha <53391715+binitshrest@users.noreply.github.com>
2025-07-12 09:48:08 +0545

    implemented key cloak, circuit breaker and time limiter

--- order-service/src/main/java/com/binitshrestha/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/binitshrestha/order_service/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.binitshrestha.order_service.controller;
 
 import com.binitshrestha.order_service.dto.OrderRequest;
 import com.binitshrestha.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +16,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some time!";
     }
 }
